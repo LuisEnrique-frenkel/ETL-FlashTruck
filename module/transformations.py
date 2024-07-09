eab6265079be04337c2ee3f0db5ff1c5516c66c0
lnoguera <lnoguera@frenkel.com.mx>
2024-07-09 12:37:23 -0600

    Correcting module scope

--- module/transformations.py
+++ module/transformations.py
@@ -1,4 +1,40 @@
+from fs.osfs import OSFS
+from fs.subfs import SubFS
+import sqlite3 as db
 import pandas as pd
+import fs
+
+def get_fulldf(SubFS: SubFS) -> pd.DataFrame:
+    """
+    Connects to SQLite databases and concatenate information into a DataFrame.
+    
+    Parameters:
+    SubFS (SubFS): Object representing file system operations.
+    
+    Returns:
+    pd.DataFrame: DataFrame containing concatenate information from databases.
+    """
+    df = pd.DataFrame()  # DataFrame para almacenar los datos combinados
+    
+    try:
+        for index, path in enumerate(SubFS.walk.files()):
+            # Create connection to the DB
+            conn = db.connect(SubFS.getsyspath(path))
+            
+            # Read data from the database
+            query = 'SELECT * FROM LoggedProcessValue;'
+            data = pd.read_sql_query(query, conn)
+            
+            # Concatenate the data to df
+            df = pd.concat([df, data], ignore_index=True)
+            
+            # Close the connection
+            conn.close()
+            
+    except Exception as e:
+        print(f"Error processing database at {path}: {str(e)}")
+    
+    return df
 
 def get_full_steam(path_LoggingTag: str, path_TLG: str, TLG: int) -> pd.DataFrame:
     """ Get join from logging tag and the data frame values 
