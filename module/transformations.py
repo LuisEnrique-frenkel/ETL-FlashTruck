54710e8bd9955c701b1e5c425e589ec598290e72
lnoguera <lnoguera@frenkel.com.mx>
2024-07-22 11:03:05 -0600

    New database

--- module/transformations.py
+++ module/transformations.py
@@ -36,18 +36,18 @@ def get_fulldf(SubFS: SubFS) -> pd.DataFrame:
     
     return df
 
-def get_full_steam(path_LoggingTag: str, path_TLG: str, TLG: int) -> pd.DataFrame:
+def get_full_steam(path_LoggingTag_notna: str, path_TLG: str, TLG: int) -> pd.DataFrame:
     """ Get join from logging tag and the data frame values 
     Args:
-    - path_LoggingTag: Path for the LoggingTag csv file
+    - path_LoggingTag_notna: Path for the LoggingTag_notna csv file
     - path_TLG: Path for the TLG csv file
     - TLG: The number of the TLG
     
     Returns:
     - df: df of the inner join
     """
-    df_LoggingTag = pd.read_csv(path_LoggingTag)
-    df_LoggingTag = df_LoggingTag[["pk_Key", "tag_name", "fk_LogId", "DataType"]]
+    df_LoggingTag_notna = pd.read_csv(path_LoggingTag_notna)
+    df_LoggingTag_notna = df_LoggingTag_notna[["pk_Key", "tag_name", "fk_LogId", "DataType"]]
     df_TLG = pd.read_csv(path_TLG)
-    return pd.merge(df_TLG, df_LoggingTag[df_LoggingTag['fk_LogId'] == TLG], 
+    return pd.merge(df_TLG, df_LoggingTag_notna[df_LoggingTag_notna['fk_LogId'] == TLG], 
                         left_on='pk_fk_Id', right_on='pk_Key', how='inner')
